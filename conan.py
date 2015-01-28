f163ba94bfc2395d4a9ad4e511f77d0639543e84
Clemens Horch <mail@clemenshorch.de>
2015-01-28 19:30:36 +0100

    Added conan.py preprocessor for automatic line wrapping. Added linenumbers option.

---
+++ conan.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+
+__author__ = "Clemens Horch"
+__copyright__ = "Copyright 2015, Clemens Horch"
+__license__ = "MIT"
+__email__ = "mail@clemenshorch.de"
+
+import re
+import os
+
+class ConAn:
+	def __init__(self, label, linelength):
+		self.linelength = linelength
+		self.longestspeaker = ''
+		self.turns = []
+		self.output = ''
+		self.label = label
+		self.lastspeaker = ''
+
+	def getbracket(self, idx):
+		if idx == 0:
+			return ''
+		elif idx % 2 == 1:
+			return '['
+		else:
+			return ']'
+
+	def output_alone(self, speaker, text):
+		if speaker == self.lastspeaker:
+			speaker = ''
+		else:
+			self.lastspeaker = speaker
+		self.output += '\t\\alone{' + speaker + '}{'+ text + '}\n'
+
+
+	def output_simul(self, As, At, Bs, Bt):
+		if At == '':
+			self.output_alone(Bs, Bt)
+		elif Bt == '':
+			self.output_alone(As, At)
+		else:
+			if As == self.lastspeaker:
+				As = ''
+			self.output += '\t\simul{' + As + '}{' + At + '}{' + Bs + '}{' + Bt + '}\n'
+			self.lastspeaker = Bs	
+			
+
+	def append(self, txt):
+		# process lines
+		for line in txt.splitlines():
+			if line.strip() == '':
+				self.turns.append(['', ''])
+				continue
+
+			#split speaker and text
+			speaker, text =  line.split(':', 1)
+			speaker = speaker.strip()
+
+			# find longest speaker name
+			if len(speaker) > len(self.longestspeaker):
+				self.longestspeaker = speaker
+
+			# split into overlap groups
+			text = text.strip()
+			text = re.split('[\[\]]', text)
+
+			self.turns.append([speaker, text])
+
+	def process(self):
+		textlen = self.linelength - len(self.longestspeaker) - 2
+		self.output = '\\begin{conan}[' + self.longestspeaker + ']\n'
+
+		turns_iter = enumerate(self.turns)
+		for tidx, turn in turns_iter:
+			# empty lines
+			if turn[0] == '':
+				continue
+
+			# no overlap
+			if len(turn[1]) == 1:
+				self.output_simul(turn[0], turn[1][0], '', '')
+				continue
+
+			# overlap
+			A = turn
+			B = self.turns[tidx + 1]
+			Ao = ''
+			Bo = ''
+
+			# loop over all overlap groups
+			for i,_ in enumerate(A[1]):
+				A[1][i] = self.getbracket(i) + A[1][i]
+				B[1][i] = self.getbracket(i) + B[1][i]
+				chunkA = A[1][i].split()
+				chunkB = B[1][i].split()
+
+				# loop over all words in a overlap group
+				while(len(chunkA) > 0 or len(chunkB) > 0):
+					# get next word
+					nextA = ''
+					nextB = ''
+					if len(chunkA) > 0:
+						nextA = chunkA[0]
+					if len(chunkB) > 0:
+						nextB = chunkB[0]
+
+					#  flush line, if too long
+					if len(Ao) + len(nextA) > textlen or len(Bo) + len(nextB) > textlen:
+						self.output_simul(A[0], Ao, B[0], Bo)				
+						Ao = ''
+						Bo = ''
+
+					# append next word to line
+					if len(chunkA) > 0:
+						Ao += chunkA.pop(0) + ' '
+						
+					if len(chunkB) > 0:
+						Bo += chunkB.pop(0) + ' '
+						
+			self.output_simul(A[0], Ao, B[0], Bo)	
+
+			# skip next turn
+			next(turns_iter, None)
+
+		if self.label:
+			self.output += '\t\label{' + self.label + '}\n'
+		self.output += '\end{conan}\n'
+
+def process_file(filename):
+	label = filename.replace(os.path.sep, '.')
+	label = label.replace('..', '', 1)
+
+	conan = ConAn(label, 66)
+
+	with open(filename, 'rb') as f:
+		conan.append(f.read())
+
+	print 'processing ' + filename
+	conan.process()
+	#print(conan.output)
+
+	with open(filename + '.tex', 'wb') as f:
+		f.write(conan.output)
+
+
+if __name__ == "__main__":
+	for root, dir, files in os.walk(u'.'):
+		for f in files:
+			name = os.path.join(root, f)
+			if not name.endswith('.conan'):
+				continue
+			process_file(name)
+
